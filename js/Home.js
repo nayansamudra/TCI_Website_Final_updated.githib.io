f1c9876848299fabfa37f88bb55af2719e490009
Samudra Gupta <113692208+nayansamudra@users.noreply.github.com>
2023-03-01 13:52:31 +0530

    Add files via upload

--- js/Home.js
+++ js/Home.js
@@ -138,26 +138,202 @@ $(document).ready(function () {
 
 
   // wrap.on("scroll", function (e) {
-  //   scroll_counter += 1;
-  //   if (this.scrollLeft < 1000 && this.scrollLeft > 0) {
-  //     console.log(scroll_counter);
-  //     $(this).scrollLeft(0)
-  //     if (scroll_counter > 5) {
-  //       $(this).scrollLeft(1000)
-  //     }
-  //   } else if (this.scrollLeft < 2000 && this.scrollLeft > 1000) {
-  //     console.log(scroll_counter);
-  //     $(this).scrollLeft(1000)
-  //     if (scroll_counter > 10) {
-  //       $(this).scrollLeft(2000)
-  //     }
-  //   } else if (this.scrollLeft < 3000 && this.scrollLeft > 2000) {
-  //     console.log(scroll_counter);
-  //     $(this).scrollLeft(2000)
-  //     if (scroll_counter > 15) {
-  //       $(this).scrollLeft(3000)
-  //     }
+  //   // scroll_counter += 1;
+  //   // if (this.scrollLeft < 1000 && this.scrollLeft > 0) {
+  //   //   console.log(scroll_counter);
+  //   //   $(this).scrollLeft(0)
+  //   //   if (scroll_counter > 5) {
+  //   //     $(this).scrollLeft(1000)
+  //   //   }
+  //   // } else if (this.scrollLeft < 2000 && this.scrollLeft > 1000) {
+  //   //   console.log(scroll_counter);
+  //   //   $(this).scrollLeft(1000)
+  //   //   if (scroll_counter > 10) {
+  //   //     $(this).scrollLeft(2000)
+  //   //   }
+  //   // } else if (this.scrollLeft < 3000 && this.scrollLeft > 2000) {
+  //   //   console.log(scroll_counter);
+  //   //   $(this).scrollLeft(2000)
+  //   //   if (scroll_counter > 15) {
+  //   //     $(this).scrollLeft(3000)
+  //   //   }
+  //   // }
+  // });
+
+  // $(window).scroll(function(event) {
+  //   // console.log($("#layoutbox").offset().top < $(window).scrollTop() + $(window).outerHeight());
+  // });
+
+  //   $(document).scroll(function(){
+  //     $('div').find('#layoutbox').each(function(){
+  //         var centerLine = $(window).height()/2;
+  //         var divStart = $(this).offset().top;
+  //         var divEnd = divStart + $(this).height();
+  //         if(divStart < centerLine && divEnd > centerLine){
+  //             //do the thing
+  //             console.log('u r at center')
+  //         } else {
+  //             //undo the thing
+  //             console.log('u r not at center')
+  //         };
+  //     });
+  // });
+
+
+  // gsap.registerPlugin(ScrollTrigger);
+
+  // gsap.set(".massiveImage", { backgroundImage: `url(https://source.unsplash.com/random/${innerWidth * 3}x${innerHeight})` })
+
+  // gsap.to(".massiveImage", {
+  //   xPercent: -100,
+  //   x: () => innerWidth,
+  //   ease: "none",
+  //   scrollTrigger: {
+  //     trigger: ".massiveImage",
+  //     start: "center center",
+  //     end: () => innerWidth * 3,
+  //     scrub: true,
+  //     pin: true,
+  //     invalidateOnRefresh: true,
+  //     anticipatePin: 1
   //   }
+  // });
+
+  // gsap.registerPlugin(ScrollTrigger);
+
+  // let sections = gsap.utils.toArray(".panel");
+
+  // let scrollTween = gsap.to(sections, {
+  //   xPercent: -100 * (sections.length - 1),
+  //   ease: "none", // <-- IMPORTANT!
+  //   scrollTrigger: {
+  //     trigger: "#container",
+  //     start: "center center",
+  //     end: "right right",
+  //     pin: true,
+  //     scrub: 0.1,
+  //     // snap: directionalSnap(1 / (sections.length - 1)),
+  //     end: "+=3000"
+  //   }
+  // });
+
+  // gsap.set(".box-1, .box-2", { y: 100 });
+  // ScrollTrigger.defaults({ markers: { startColor: "white", endColor: "white" } });
+
+  // // red section
+  // gsap.to(".box-1", {
+  //   y: -130,
+  //   duration: 2,
+  //   ease: "elastic",
+  //   scrollTrigger: {
+  //     trigger: ".box-1",
+  //     containerAnimation: scrollTween,
+  //     // start: "left center",
+  //     toggleActions: "play none none reset",
+  //     id: "1",
+  //   }
+  // });
+
+
+  // // gray section
+  // gsap.to(".box-2", {
+  //   y: -120,
+  //   backgroundColor: "#1e90ff",
+  //   ease: "none",
+  //   scrollTrigger: {
+  //     trigger: ".box-2",
+  //     containerAnimation: scrollTween,
+  //     // start: "center 80%",
+  //     // end: "center 20%",
+  //     scrub: true,
+  //     id: "2"
+  //   }
+  // });
+
+  // // purple section
+  // ScrollTrigger.create({
+  //   trigger: ".box-3",
+  //   containerAnimation: scrollTween,
+  //   toggleClass: "active",
+  //   // start: "center 60%",
+  //   id: "3"
+  // });
+
+  // // purple section
+  // ScrollTrigger.create({
+  //   trigger: ".box-4",
+  //   containerAnimation: scrollTween,
+  //   toggleClass: "active",
+  //   start: "center 60%",
+  //   end: "right bottom",
+  //   id: "4"
+  // });
+
+  // // green section
+  // ScrollTrigger.create({
+  //   trigger: ".green",
+  //   containerAnimation: scrollTween,
+  //   start: "center 65%",
+  //   end: "right right",
+  //   onEnter: () => console.log("enter"),
+  //   onLeave: () => console.log("leave"),
+  //   onEnterBack: () => console.log("enterBack"),
+  //   onLeaveBack: () => console.log("leaveBack"),
+  //   onToggle: self => console.log("active", self.isActive),
+  //   id: "4"
+  // });
+
+  // // only show the relevant section's markers at any given time
+  // gsap.set(".gsap-marker-start, .gsap-marker-end, .gsap-marker-scroller-start, .gsap-marker-scroller-end", { autoAlpha: 0 });
+  // ["red", "gray", "purple", "green"].forEach((triggerClass, i) => {
+  //   ScrollTrigger.create({
+  //     trigger: "." + triggerClass,
+  //     containerAnimation: scrollTween,
+  //     start: "left 30%",
+  //     end: i === 3 ? "right right" : "right 30%",
+  //     markers: false,
+  //     onToggle: self => gsap.to(".marker-" + (i + 1), { duration: 0.25, autoAlpha: self.isActive ? 1 : 0 })
+  //   });
+  // });
+
+  // // helper function for causing the sections to always snap in the direction of the scroll (next section) rather than whichever section is "closest" when scrolling stops.
+  // // function directionalSnap(increment) {
+  // //   let snapFunc = gsap.utils.snap(increment);
+  // //   return (raw, self) => {
+  // //     let n = snapFunc(raw);
+  // //     return Math.abs(n - raw) < 1e-4 || (n < raw) === self.direction < 0 ? n : self.direction < 0 ? n - increment : n + increment;
+  // //   };
+  // // }
+
+  // $(function () { // wait for document ready
+  //   // init
+  //   var controller = new ScrollMagic.Controller();
+
+  //   // define movement of panels
+  //   var wipeAnimation = new TimelineMax()
+  //     // animate to second panel
+  //     .to("#slideContainer", 0.5, { z: -150 })		// move back in 3D space
+  //     .to("#slideContainer", 1, { x: "-25%" })	    // move in to first panel
+  //     .to("#slideContainer", 0.5, { z: 0 })			// move back to origin in 3D space
+  //     // animate to third panel
+  //     .to("#slideContainer", 0.5, { z: -150, delay: 1 })
+  //     .to("#slideContainer", 1, { x: "-50%" })
+  //     .to("#slideContainer", 0.5, { z: 0 })
+  //     // animate to forth panel
+  //     .to("#slideContainer", 0.5, { z: -150, delay: 1 })
+  //     .to("#slideContainer", 1, { x: "-75%" })
+  //     .to("#slideContainer", 0.5, { z: 0 });
+
+  //   // create scene to pin and link animation
+  //   new ScrollMagic.Scene({
+  //     triggerElement: "#pinContainer",
+  //     triggerHook: "onLeave",
+  //     duration: "500%"
+  //   })
+  //     .setPin("#pinContainer")
+  //     .setTween(wipeAnimation)
+  //     .addIndicators() // add indicators (requires plugin)
+  //     .addTo(controller);
   // });
 
 
